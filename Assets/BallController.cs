303a75c4d364ebafbd14082500b7f2b3b09fa64e
Ronit <butironit@gmail.com>
2025-01-17 22:11:21 -0800

    Add InputManager for player input handling and update BallController to use serialized fields

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
 
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] float ballSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
+    }
+
+    public void MoveBall(Vector2 input)
+    {
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
     // Update is called once per frame
