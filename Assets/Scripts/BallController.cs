786a15d57e85b66c0fca8d59176a75a95dc2a771
Ronit <butironit@gmail.com>
2025-01-17 22:52:28 -0800

    Enhance BallController with jump functionality

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,29 +2,60 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-
     [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] float ballSpeed;
+    [SerializeField] float ballSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isGrounded = false;
+
     void Start()
     {
-
+        // Ensure we have a reference to the Rigidbody
+        if (sphereRigidbody == null)
+        {
+            sphereRigidbody = GetComponent<Rigidbody>();
+        }
     }
 
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+
+        // Handle jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;  // Immediately set to false when jumping
+        }
     }
 
-    // Update is called once per frame
+    // Called when the collider enters another collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is with the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Called when the collider exits another collider
+    private void OnCollisionExit(Collision collision)
+    {
+        // When leaving the ground, update grounded state
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
     void Update()
     {
-        Vector2 inputVector = Vector2.zero; // initialize our input vector to zero
+        Vector2 inputVector = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
+            inputVector += Vector2.up;
         }
         if (Input.GetKey(KeyCode.S))
         {
@@ -41,8 +72,12 @@ public class BallController : MonoBehaviour
 
         Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
-        Debug.Log("Resultant Vector: " + inputVector);
-        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
 
+        // Handle jumping in Update as well for better responsiveness
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
